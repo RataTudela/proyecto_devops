4dc460a2661108f32a6b5842a6dff01eed197610
RataTudela <ben.olavarria@duocuc.cl>
2026-05-16 18:25:44 -0400

    Probando integracion

--- proyecto semestral/back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/SpringbootApiRestDespachoApplication.java
+++ proyecto semestral/back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/SpringbootApiRestDespachoApplication.java
@@ -2,7 +2,7 @@ package com.citt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Prueba de pipeline GitHub Actions - Commit de prueba
 @SpringBootApplication
 public class SpringbootApiRestDespachoApplication {
 
